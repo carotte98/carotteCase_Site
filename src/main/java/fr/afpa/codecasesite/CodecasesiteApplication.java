170e7629325caf9af96177d265a3b1a1d80a4fa5
Thierry <thierrymordant@ymail.com>
2026-04-22 14:00:48 +0200

    <test> Test CICD 5

--- src/main/java/fr/afpa/codecasesite/CodecasesiteApplication.java
+++ src/main/java/fr/afpa/codecasesite/CodecasesiteApplication.java
@@ -24,6 +24,7 @@ public class CodecasesiteApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception{
 		System.out.println(props.getApiUrl());
 	}
-	
 
+	// fe
+	
 }
